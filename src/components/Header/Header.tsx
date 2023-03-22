22166f007275d6ca8f563af1faaaa3fec67cbedf
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-03-23 03:27:50 +0900

    refactor(header): delete unused code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,48 +1,8 @@
-import { createStyles } from "@mantine/core";
-
 import React from "react";
 
 import LoginModalContainer from "@/components/Header/LoginModal/loginModalContainer";
 
 import NavBarContainer from "./NavBar/navBarContainer";
-
-const useStyles = createStyles((theme, _params, getRef) => ({
-  wrapper: {
-    backgroundColor:
-      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-    // maxWidth: 1190,
-    width: "100vw",
-    height: 60,
-    zIndex: 6,
-    position: "sticky",
-    textAlign: "center",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    top: 0,
-    left: 0,
-    right: 0,
-    marginLeft: 0,
-    marginRight: 0,
-    borderBottom: `1px solid ${
-      theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[2]
-    }`,
-  },
-  navWrapper: {
-    maxWidth: 1130,
-    width: "63vw",
-    display: "flex",
-  },
-  title: {
-    ref: getRef("child"),
-    maxWidth: 1130,
-    color: theme.colorScheme === "dark" ? theme.white : theme.black,
-    fontSize: theme.fontSizes.lg,
-    display: "flex",
-    alignItems: "center",
-  },
-  form: {},
-}));
 
 const Header = () => {
   return (
@@ -50,7 +10,6 @@ const Header = () => {
       <NavBarContainer />
       <LoginModalContainer />
     </>
-
   );
 };
 
